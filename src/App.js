4d8a42514f9f6d9b0f234d0f99fd880ad722dafc
jaswanth-b-kumar <124692437+jaswanth-b-kumar@users.noreply.github.com>
2023-09-29 17:20:04 +0530

    home header and footer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import './Styles/styles.scss'
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Jaswanth's Resume</h1>
-    </div>
+
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='*' element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="header-component">
+        Header
+    </div>
+  )
+}
+
+export default Header
