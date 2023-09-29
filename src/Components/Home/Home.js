249a173e580419273753a7ba7ed1f70ee3dabc70
jaswanth-b-kumar <124692437+jaswanth-b-kumar@users.noreply.github.com>
2023-09-30 00:02:04 +0530

    home page added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,10 +5,10 @@ function Home() {
   return (
     <div className="container about-container">
       <div className="row">
-        <div className="col-9 col-sm-3 col-md-3 col-lg-3 text-center mx-auto mt-5">
+        <div className="col-11 col-sm-3 col-md-3 col-lg-3 text-center mx-auto mt-5">
           <img src={profilePhoto} className="img-fluid rounded col-6 img-thumbnail" alt='profile' />
         </div>
-        <div className="col-9 col-sm-11 col-md-11 col-lg-11 mx-auto mt-5">
+        <div className="col-11 col-sm-11 col-md-11 col-lg-11 mx-auto mt-5">
           <h2 className='about-head'>Jaswanth Kumar Bevara</h2>
           <h4 className='about-designation'>Advanced Application Engineering Analyst at Accenture</h4>
           <h6 className='about-role'>I am a skilled UI developer with a passion for crafting engaging and user-friendly digital experiences.</h6>
