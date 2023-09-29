24132482b536031265d5199e78c19100af0ec371
jaswanth-b-kumar <124692437+jaswanth-b-kumar@users.noreply.github.com>
2023-09-29 21:46:07 +0530

    header css

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
 import './Styles/styles.scss'
 import Footer from "./Components/Footer/Footer";
+import Experience from "./Components/Experience/Experience";
+import Contact from "./Components/Contact/Contact";
+import Projects from "./Components/Projects/Projects";
 
 function App() {
   return (
 
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='*' element={<Home />} />
-      </Routes>
-      <Footer />
+      <div className="app-component">
+        <Header />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='*' element={<Home />} />
+          <Route path='/experience' element={<Experience />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/projects' element={<Projects />} />
+        </Routes>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import './Header.scss'
 
 function Header() {
-  return (
-    <div className="header-component">
-        Header
-    </div>
-  )
+    let [navActive,setNavActive] = useState(false);
+
+    const handleNavBtn = () => {
+        setNavActive(!navActive)
+    }
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light navbar-custom-css">
+            <h1 className="navbar-brand portfolio-logo blockquote">Jaswanth Kumar Bevara</h1>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded={navActive ? "True" : "False"} aria-label="Toggle navigation" onClick={handleNavBtn}>
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className={`${navActive ? '' : 'collapse'} navbar-collapse justify-content-end nav-pills`} onClick={handleNavBtn} id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto nav-ul-custom">
+                    <li className="nav-item"><NavLink className="nav-link" to='/'><span>Home</span></NavLink></li>
+                    <li className="nav-item"><NavLink className="nav-link" to='/experience'><span>Experience</span></NavLink></li>
+                    <li className="nav-item"><NavLink className="nav-link" to='/projects'><span>Projects</span></NavLink></li>
+                    <li className="nav-item"><NavLink className="nav-link" to='/contact'><span>Contact</span></NavLink></li>
+                </ul>
+            </div>
+        </nav>
+        
+    )
 }
 
 export default Header
