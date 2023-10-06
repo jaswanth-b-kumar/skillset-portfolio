e16963ca39d05283a8e9801c2d2f94376e4be2f2
jaswanth-b-kumar <124692437+jaswanth-b-kumar@users.noreply.github.com>
2023-10-06 23:44:54 +0530

    skill added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -93,6 +93,14 @@ function Home() {
                 <p>JQuery</p>
               </div>
             </div>
+            <div className='skill-container col-6 col-lg-4'>
+              <div className='skill-background  d-flex align-items-center'>
+                <div className='skill-icon d-flex justify-content-center align-items-center'>
+                  <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><path d="M288 288h-32v-64h32v64zm288-128v192H288v32H160v-32H0V160h576zm-416 32H32v128h64v-96h32v96h32V192zm160 0H192v160h64v-32h64V192zm224 0H352v128h64v-96h32v96h32v-96h32v96h32V192z"/></svg>
+                </div>
+                <p>npm</p>
+              </div>
+            </div>
           </div>
         </div>
         <div className='col-12 col-md-6 about-container'>
