457a14a6a53ee8bebe1dee0cd409827fd254a8ca
jaswanth <jaswanth.k.bevara@gmail.com>
2024-12-22 20:44:52 +0000

    Cleaning repo for new portfolio design

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
-import Header from "./Components/Header/Header";
-import Home from "./Components/Home/Home";
 import './Assets/Styles/styles.scss'
-import Footer from "./Components/Footer/Footer";
-import Experience from "./Components/Experience/Experience";
-import Contact from "./Components/Contact/Contact";
-import Projects from "./Components/Projects/Projects";
+import Home from './Components/Home/Home';
 
 function App() {
   return (
-
-    <HashRouter>
-      <div className="app-component">
-        <Header />
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route path='/' element={<Home />} />
-          <Route path='/experience' element={<Experience />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/projects' element={<Projects />} />
-        </Routes>
-        <Footer />
-      </div>
-    </HashRouter>
+    <Home />
   );
 }
 
