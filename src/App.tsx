553c685373f837800341b5bd649fe730a575d24e
jaswanth-b-kumar <124692437+jaswanth-b-kumar@users.noreply.github.com>
2026-04-13 23:59:09 +0100

    Feature: Added new navbar to the repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
   return (
-    <main className="app">
-      <h1 className="app__title">Hello World</h1>
-    </main>
+    <>
+      <Navbar />
+    </>
   );
 }
 
